4c58d08655b2d165a73101bfa5017912468f642f
KyleMiles <krm504@nyu.edu>
2019-05-05 15:06:18 -0400

    Change Tactics

--- ECE 4414/Project/Ver1/Ver1.ino
+++ ECE 4414/Project/Ver1/Ver1.ino
@@ -6,7 +6,8 @@
 
 
 Player* player;
-bool button_pressed_bounce = false;
+bool button_pressed_bounce_a = false;
+bool button_pressed_bounce_b = false;
 
 
 void setup()
@@ -30,42 +31,29 @@ uint8_t button_pressed()
 
 void loop()
 {
-  switch (button_pressed())
+  if (button_pressed() & BUTTON_HH)
   {
-
-  case BUTTON_HH:
-    if (!button_pressed_bounce)
-    {
-      Serial.println(BUTTON_HH_NAME);
+    if (!button_pressed_bounce_a)
       player->play(BUTTON_HH_NAME);
-    }
-    button_pressed_bounce = true;
-    break;
-
-  case BUTTON_SNARE:
-    if (!button_pressed_bounce)
-    {
-      Serial.println(BUTTON_SNARE_NAME);
-      player->play(BUTTON_SNARE_NAME);
-    }
-    button_pressed_bounce = true;
-    break;
-
-  case BUTTON_INVALID:
-  default:
-    // Serial.println("Doing nothing");
-    button_pressed_bounce = false;
-    // delay(100);
-    break;
-
+    button_pressed_bounce_a = true;
   }
+  else
+    button_pressed_bounce_a = false;
+  
+  if(button_pressed() & BUTTON_SNARE)
+  {
+    if (!button_pressed_bounce_b)
+      player->play(BUTTON_SNARE_NAME);
+    button_pressed_bounce_b = true;
+  }
+  else
+    button_pressed_bounce_b = false;
 
   if (player->has_error())
   {
-    while (1)
-    {
-      Serial.println("Encountered Error That Cannot Be Recovered From");
-      delay(1000);
-    }
+    Serial.println("Error, re-initializing...");
+    delete player;
+    player = new Player();
+    Serial.println("Done");
   }
 }
